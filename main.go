e0a6e83f6e16ac46c1ecca43837ef50b640dfbd7
Florian Wilhelm <florian.wilhelm02@sap.com>
2023-04-26 16:36:58 +0200

    Setup dagger pipeline

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "os"
+
+    "dagger.io/dagger"
+)
+
+func main() {
+    if err := build(context.Background()); err != nil {
+        fmt.Println(err)
+    }
+}
+
+func build(ctx context.Context) error {
+    fmt.Println("Building with Dagger")
+
+    // initialize Dagger client
+    client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+    if err != nil {
+        return err
+    }
+    defer client.Close()
+
+    // get reference to the local project
+    src := client.Host().Directory(".")
+
+    // create empty directory to put build outputs
+    outputs := client.Directory()
+
+    // get `maven` image
+    maven := client.Container().From("maven:3-eclipse-temurin-17")
+
+    // mount cloned repository into `maven` image
+    maven = maven.WithDirectory("/src", src).WithWorkdir("/src")
+
+    out, err := maven.WithExec([]string{"mvn", "verify"}).Stdout(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(out)
+
+    // write build artifacts to host
+    _, err = outputs.Export(ctx, ".")
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
